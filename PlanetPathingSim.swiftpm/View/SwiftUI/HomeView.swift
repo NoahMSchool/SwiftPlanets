a5ef3cfa2444c86a871b92d31a8b8bdcfb5ba7e6
NoahMSchool <19noahm@hollandparkschool.co.uk>
2025-02-03 22:10:25 +0000

    #10 Made Start Button link to main view using navigation Stack

--- PlanetPathingSim.swiftpm/View/SwiftUI/HomeView.swift
+++ PlanetPathingSim.swiftpm/View/SwiftUI/HomeView.swift
@@ -1,34 +1,42 @@
 import SwiftUI
 
 struct HomeView : View{
+    @EnvironmentObject var galaxy : Galaxy
+    @State private var navigateToGame = false
     var body : some View{
         TitleName()
-
-        Button(action: {
-            print("start")
-        }) {
-            ZStack{
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.black)
-
-                HStack{
-                    Text("Start")
-                        .font(.custom("ChalkDuster", size: 24))
-                        .padding(.horizontal)
-                    Image(systemName: "play.fill")
-                        .font(.system(size: 24))   
-                        .foregroundColor(.white)   
+        NavigationStack{
+            
+            Button(action: {
+                navigateToGame = true
+            }) {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.black)
+                    
+                    HStack{
+                        Text("Start")
+                            .font(.custom("ChalkDuster", size: 24))
+                            .padding(.horizontal)
+                        Image(systemName: "play.fill")
+                            .font(.system(size: 24))   
+                            .foregroundColor(.white)   
+                    }
                 }
+                .frame(maxWidth: 250, maxHeight: 100)
+                
+                
+            }
+            .navigationDestination(isPresented: $navigateToGame){
+                ContentView().environmentObject(galaxy)
+            }
+            
+            Button(action: {print("Menu")}, 
+                   label: {
+                Image(systemName: "gearshape")
+                    .font(.system(size: 72))
+            })
+            
         }
-        .frame(maxWidth: 250, maxHeight: 100)
-        
-
-        }
-        Button(action: {print("Menu")}, 
-               label: {
-            Image(systemName: "gearshape")
-                .font(.system(size: 72))
-        })
-
     }
 }
