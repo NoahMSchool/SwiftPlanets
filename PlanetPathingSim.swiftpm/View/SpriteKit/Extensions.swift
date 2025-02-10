1d627c40965e2e1ecf265e8fc3157405cf582940
NoahMSchool <19noahm@hollandparkschool.co.uk>
2025-02-10 21:42:40 +0000

    #7 Improve Line Intersection algorithm
    If lines share start or end assume they are not touching

--- PlanetPathingSim.swiftpm/View/SpriteKit/Extensions.swift
+++ PlanetPathingSim.swiftpm/View/SpriteKit/Extensions.swift
@@ -19,7 +19,9 @@ func checkIntersections(p1: CGPoint, q1: CGPoint, p2: CGPoint, q2: CGPoint) -> B
         let val = (b.y - a.y) * (c.x - b.x) - (b.x - a.x) * (c.y - b.y)
         return (val > 0) ? 1 : -1 // Clockwise or Counterclockwise
     }
-    
+    if p1 == q1 || p1 == q2 || p2 == q1 || p2 == q2{
+        return false
+    }
     let o1 = orientation(p1, q1, p2)
     let o2 = orientation(p1, q1, q2)
     let o3 = orientation(p2, q2, p1)
